8adcc13d384aa56ff5c0de6e36aafd724abd2a45
Ievgen <ievgen@Ievgens-Mac.local>
2019-04-18 07:13:09 -0700

    added model files

---
+++ Dnd5eApp/Model/ContentDownloader.swift
@@ -0,0 +1,13 @@
+//
+//  ContentDownloader.swift
+//  Dnd5eApp
+//
+//  Created by Ievgen on 4/18/19.
+//  Copyright © 2019 Ievgen. All rights reserved.
+//
+
+import Foundation
+
+class ContentDownloader {
+    
+}
---
+++ Dnd5eApp/Model/ContentManager.swift
@@ -0,0 +1,14 @@
+//
+//  ContentManager.swift
+//  Dnd5eApp
+//
+//  Created by Ievgen on 4/18/19.
+//  Copyright © 2019 Ievgen. All rights reserved.
+//
+
+import Foundation
+
+class ContentManager {
+    
+    private static let sharedManager = ContentManager()
+}
