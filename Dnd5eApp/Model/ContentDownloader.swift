290a9310e6993008a9fcb11b91f913d17184ff5e
Ievgen <ievgen@Ievgens-Mac.local>
2019-04-18 07:40:29 -0700

    decomposing massive view controller

--- Dnd5eApp/Model/ContentDownloader.swift
+++ Dnd5eApp/Model/ContentDownloader.swift
@@ -8,6 +8,28 @@
 
 import Foundation
 
+enum DownloadingError: Int {
+    case incorrectURL
+    case downloadFailed
+    case invalidResponseStatusCode
+    case invalidResponseData
+}
+
 class ContentDownloader {
     
+    internal var urlSessionProtocolClasses: [AnyClass]?
+    static let spellsURLString = "http://dnd5eapi.co/api/spells"
+    
+    public func downloadContent(with completionHandler: (_ result: [String: Any], _ error: Error) -> Void) {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = self.urlSessionProtocolClasses
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        guard let spellsUrl  = URL(string: type(of: self).spellsURLString) else { return }
+        
+        session.dataTask(with: spellsUrl) { (data, response, error) in
+            guard let jsonData = data else { return }
+            let dictionary = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any]
+        }.resume()
+    }
 }
---
+++ Dnd5eApp/View/SpellDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpellDetailViewController.swift
+//  Dnd5eApp
+//
+//  Created by Ievgen on 4/18/19.
+//  Copyright © 2019 Ievgen. All rights reserved.
+//
+
+import UIKit
+
+class SpellDetailViewController: UIViewController {
+
+}
