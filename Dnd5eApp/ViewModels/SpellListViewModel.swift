23e954b72db42fd397ab9a43d9ad98f43e36b031
Ievgen Mokeiev <ievgen.mokeiev@just-eat.com>
2020-05-18 14:28:02 +0300

    draft implementation of search

--- Dnd5eApp/ViewModels/SpellListViewModel.swift
+++ Dnd5eApp/ViewModels/SpellListViewModel.swift
@@ -13,8 +13,17 @@ class SpellListViewModel: ObservableObject {
 
     @Published var spellDTOs: [SpellDTO] = []
 
-    @Published var searchTerm: String = ""
+    @Published var searchTerm: String = "" {
+        didSet {
+            searchTermPublisher = filteredSpellListPublisher(for: publisher, by: searchTerm)
+            searchTermPublisher?
+                .replaceError(with: [])
+                .assign(to: \.spellDTOs, on: self)
+                .store(in: &cancellableSet)
+        }
+    }
 
+    private var searchTermPublisher: AnyPublisher<[SpellDTO], Error>?
     private let publisher: AnyPublisher<[SpellDTO], Error>
     private var cancellableSet: Set<AnyCancellable> = []
 
@@ -27,6 +36,21 @@ class SpellListViewModel: ObservableObject {
             .replaceError(with: [])
             .assign(to: \.spellDTOs, on: self)
             .store(in: &cancellableSet)
+    }
+
+    // TODO: - Move those to another place
+    func filteredSpellListPublisher(for publisher: AnyPublisher<[SpellDTO], Error>, by searchTerm: String) -> AnyPublisher<[SpellDTO], Error> {
+        return publisher
+            .map { self.filteredSpells(spells: $0, by: searchTerm) }
+            .eraseToAnyPublisher()
+    }
+
+    func filteredSpells(spells: [SpellDTO], by searchTerm: String) -> [SpellDTO] {
+        if searchTerm.isEmpty {
+            return spells
+        } else {
+            return spells.filter { $0.name.starts(with: searchTerm) }
+        }
     }
 }
 
