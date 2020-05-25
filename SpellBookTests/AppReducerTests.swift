9409ac8843c05cf4812a1ab5b0f6c1814f6f9fc7
Ievgen Mokeiev <ievgen.mokeiev@just-eat.com>
2020-05-25 17:42:03 +0300

    adding more tests

--- SpellBookTests/AppReducerTests.swift
+++ SpellBookTests/AppReducerTests.swift
@@ -12,12 +12,40 @@ import XCTest
 class AppReducerTests: XCTestCase {
 
     func test_reduce_to_spell_list() {
-        let store = AppStore(initialState: .init(), reducer: appReducer, environment: FakeServiceContainer(), factory: ViewFactory())
-
+        let store = makeSUT()
         let spells = FakeDataFactory.provideFakeSpellListDTO()
-
         store.send(.showSpellList(spells: spells))
-
         XCTAssertTrue(store.state.allSpells == spells)
+    }
+
+    func test_reduce_to_spell() {
+        let store = makeSUT()
+        let spell = FakeDataFactory.provideFakeSpellDTO()
+        store.send(.showSpell(spell: spell))
+        XCTAssertTrue(store.state.selectedSpell == spell)
+    }
+
+    func test_reduce_to_favorites() {
+        let store = makeSUT()
+        let spells = FakeDataFactory.provideFakeFavoritesListDTO()
+        store.send(.showFavorites(spells: spells))
+        XCTAssertTrue(store.state.favoriteSpells == spells)
+    }
+
+    func test_reduce_to_error() {
+        let store = makeSUT()
+        let error = NetworkClientError.invalidURL as Error
+        store.send(.showError(error: error))
+        guard let convertedError = store.state.error as? NetworkClientError else { XCTFail("wrong error"); return }
+        switch convertedError {
+        case .invalidURL:
+            return
+        default:
+            XCTFail("wrong error")
+        }
+    }
+
+    private func makeSUT() -> AppStore {
+        return AppStore(initialState: .init(), reducer: appReducer, environment: FakeServiceContainer(), factory: ViewFactory())
     }
 }
