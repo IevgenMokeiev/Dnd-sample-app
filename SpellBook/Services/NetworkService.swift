0d10e40c2bdf754c284bb64878c1633787d43d56
Eugene Mokeiev <ievgen.mokeiev@takeaway.com>
2022-02-14 12:24:26 +0200

    Async / await implementation

--- SpellBook/Services/NetworkService.swift
+++ SpellBook/Services/NetworkService.swift
@@ -13,43 +13,79 @@ typealias NetworkSpellPublisher = AnyPublisher<[SpellDTO], NetworkClientError>
 typealias NetworkSpellDetailPublisher = AnyPublisher<SpellDTO, NetworkClientError>
 
 struct Response: Codable {
-    public let results: [SpellDTO]
+  public let results: [SpellDTO]
 }
 
 private enum Endpoints: String {
-    case spellList = "http://dnd5eapi.co/api/spells"
-    case spellDetails = "http://dnd5eapi.co"
+  case spellList = "http://dnd5eapi.co/api/spells"
+  case spellDetails = "http://dnd5eapi.co"
 }
 
 /// Service responsible for network communication
 protocol NetworkService {
-    func spellListPublisher() -> NetworkSpellPublisher
-    func spellDetailPublisher(for path: String) -> NetworkSpellDetailPublisher
+  func spellListPublisher() -> NetworkSpellPublisher
+  func spellDetailPublisher(for path: String) -> NetworkSpellDetailPublisher
 }
 
 class NetworkServiceImpl: NetworkService {
 
-    let networkClient: NetworkClient
+  let networkClient: NetworkClient
 
-    init(networkClient: NetworkClient) {
-        self.networkClient = networkClient
+  init(networkClient: NetworkClient) {
+    self.networkClient = networkClient
+  }
+
+  func spellListPublisher() -> NetworkSpellPublisher {
+    guard let url = URL(string: Endpoints.spellList.rawValue) else {
+      return Fail(error: .invalidURL).eraseToAnyPublisher()
     }
-    
-    func spellListPublisher() -> NetworkSpellPublisher {
-        guard let url = URL(string: Endpoints.spellList.rawValue) else {
-            return Fail(error: .invalidURL).eraseToAnyPublisher()
+
+    return Future<[SpellDTO], NetworkClientError> { promise in
+      Task {
+        do {
+          let response = try await self.networkClient.performRequest(to: url, expectedType: Response.self)
+          promise(.success(response.results))
+        } catch {
+          let mappedError = { () -> NetworkClientError in
+            switch error {
+            case is DecodingError:
+              return .decodingFailed
+            case is URLError:
+              return .sessionFailed(error)
+            default:
+              return .other(error)
+            }
+          }()
+          promise(.failure(mappedError))
         }
+      }
+    }.eraseToAnyPublisher()
+  }
 
-        return networkClient.performRequest(to: url, expectedType: Response.self)
-            .map { $0.results }
-            .eraseToAnyPublisher()
+  func spellDetailPublisher(for path: String) -> NetworkSpellDetailPublisher {
+    guard let url = URL(string: Endpoints.spellDetails.rawValue + path) else {
+      return Fail(error: .invalidURL).eraseToAnyPublisher()
     }
-    
-    func spellDetailPublisher(for path: String) -> NetworkSpellDetailPublisher {
-        guard let url = URL(string: Endpoints.spellDetails.rawValue + path) else {
-            return Fail(error: .invalidURL).eraseToAnyPublisher()
+
+    return Future<SpellDTO, NetworkClientError> { promise in
+      Task {
+        do {
+          let response = try await self.networkClient.performRequest(to: url, expectedType: SpellDTO.self)
+          promise(.success(response))
+        } catch {
+          let mappedError = { () -> NetworkClientError in
+            switch error {
+            case is DecodingError:
+              return .decodingFailed
+            case is URLError:
+              return .sessionFailed(error)
+            default:
+              return .other(error)
+            }
+          }()
+          promise(.failure(mappedError))
         }
-
-        return networkClient.performRequest(to: url, expectedType: SpellDTO.self)
-    }
+      }
+    }.eraseToAnyPublisher()
+  }
 }
--- SpellBook/Utilities/PublisherExtensions.swift
+++ SpellBook/Utilities/PublisherExtensions.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2022 Ievgen. All rights reserved.
 //
 
-import Foundation
+import Combine
+
+extension Publisher {
+  func asyncMap<T>(
+    _ transform: @escaping (Output) async throws -> T
+  ) -> Publishers.FlatMap<Future<T, Error>, Publishers.SetFailureType<Self, Error>> {
+    flatMap { value in
+      Future { promise in
+        Task {
+          do {
+            let output = try await transform(value)
+            promise(.success(output))
+          } catch {
+            promise(.failure(error))
+          }
+        }
+      }
+    }
+  }
+}
